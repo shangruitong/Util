ddacb6fed526cc46e580ddb421e08c5c92f54856
shangruitong <541342084@qq.com>
2018-09-07 16:06:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ util.toDecimal = require('./lib/toDecimal.js');
 // 精确到小数点n位，不需要额外的末尾的0
 util.toDecimalNoZero = require('./lib/toDecimalNoZero.js');
 
+// 截取指定长度的中英文字符串，中文算2个长度
+util.subString = require('./lib/subString.js');
+
 module.exports = util;
