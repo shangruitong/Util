655b7dd44b1f545eb0f4fafaced70fb2704d4b8b
shangruitong <541342084@qq.com>
2018-09-07 16:45:13 +0800

    Update toDecimalNoZero.js

--- src/lib/toDecimalNoZero.js
+++ src/lib/toDecimalNoZero.js
@@ -1,5 +1,5 @@
 module.exports = function toDecimalNoZero(x, n) {
-    n = n || 1;
+    n = typeof n === 'undefined' ? 1 : n;
     var ratio = Math.pow(10, n);
     var f = Math.round(x * ratio) / ratio;
     var s = f.toString();
