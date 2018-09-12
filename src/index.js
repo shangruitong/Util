ea023d65c0546923fad00f288f035cce153cf407
shangruitong <541342084@qq.com>
2018-09-12 11:15:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,4 +9,10 @@ util.toDecimalNoZero = require('./lib/toDecimalNoZero.js');
 // 截取指定长度的中英文字符串，中文算2个长度
 util.subString = require('./lib/subString.js');
 
+// 数组中是否包含改元素，基础类型判断值，高级类型判断引用，返回index；不含返回-1；
+util.indexOfArray = require('./lib/indexOfArray.js');
+
+// 数组中是否包含改元素，基础类型判断值，高级类型判断引用，返回true／false；
+util.inArray = require('./lib/inArray.js');
+
 module.exports = util;
